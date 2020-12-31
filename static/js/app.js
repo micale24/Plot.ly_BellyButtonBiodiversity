adf7c8506e8c4c8d97c146fecb95d06c18d0a8af
micale24 <kaihall.usaf@gmail.com>
2020-12-31 04:55:30 -0500

    drop down menu entry completed

--- static/js/app.js
+++ static/js/app.js
@@ -1,33 +1,30 @@
 //Read in data
-
+const url = ("./static/samples.json")
 
 //Horizonatal Bar Chart with drop down menu to display the top 10 OTUs
         // Use sample_value for bar chart--otu_ids for lables-- otu_labels for hover text
 
-
-
-
-//  d3.json("/samples.json", function(samples) {
-// //             console.log(samples);
-// //         });
- 
-// const url = ("samples.json") 
-
-// // Fetch the JSON data and console log it
-// d3.json(url).then(function(data) {
-//     console.log(data);
-//  });
-        
-// // Promise Pending
-// const dataPromise = d3.json(url);
-//     console.log("Data Promise: ", dataPromise);
-        
-
-
-
- d3.json("/samples.json").then(function(data){
-
-    console.log(data[0]);
+//Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+    console.log(data);
+    var dropContent = data.names
  });
+ 
+d3.selectAll("#selDataset").on("change", optionChanged);
+
+
+// This function is called when a dropdown menu item is selected
+function optionChanged() {
+        // Use D3 to select the dropdown menu
+        var dropdownMenu = d3.select("#selDataset");
+        // Assign the value of the dropdown menu option to a variable
+        var dataset = dropdownMenu.property("value");
+        
+        //For Loop to input the test subject number into list
+
+        for (i=0, i)
+       
+      }
+
 
  
