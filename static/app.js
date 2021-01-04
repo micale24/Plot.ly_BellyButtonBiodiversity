0fdb5a51f63ab11e531779d216aad8ea0317729a
micale24 <kaihall.usaf@gmail.com>
2021-01-04 04:18:00 -0500

    re-arranged folders and files

---
+++ static/app.js
@@ -0,0 +1,10 @@
+//Read in json sample data
+d3.json("/samples.json").then((importedData) => {
+    console.log(importedData);
+})
+//Drop down list
+
+//select the html of the drop down list
+
+//append the "option" tag with the name from the json 
+
