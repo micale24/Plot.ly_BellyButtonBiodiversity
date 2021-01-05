ddd860411ccaefa858ae26c6921eb5991a886afa
micale24 <kaihall.usaf@gmail.com>
2021-01-05 04:30:50 -0500

    intit function part 2

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,61 @@
-//Read in json sample data
-d3.json("./samples.json").then((importedData) => {
-     console.log(importedData);
-     var data = importedData
-     var option = d3.select("#selDataset");
-     var names = data.names;
-     console.log(names)
 
-     names.id.forEach(element => {
+
+
+
+
+
+//Initialization funcation to insert dropdown options
+function initialization() {
+    // Selecting tag in index.html
+    var dropdown = d3.select("#selDataset");
+    // Reading data from samples.json
+    d3.json("./samples.json").then((importedData) => {
+        console.log(importedData)
+        //
+        importedData.names.forEach((name) => {
+                dropdown.append("option").text(name).property("value");
+            });
+
+        //Callback functions for plots and demographic info
+
+    });
+}
+
+initialization();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Read in json sample data
+// d3.json("./samples.json").then((importedData) => {
+//      console.log(importedData);
+//      var data = importedData
+//      var option = d3.select("#selDataset");
+//      var names = data.names;
+//      console.log(names)
          
-     });
-         
-});
+// });
     
      
      
