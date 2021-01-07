9fbed048dfc115be69eb75d5b7859641c1ce51da
micale24 <kaihall.usaf@gmail.com>
2021-01-07 11:25:58 -0500

    started bar chart function/finished bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -9,6 +9,11 @@ d3.json("./samples.json").then((data) => {
     
 });
 
+function optionChanged(id){
+    demographicPanel(id);
+    Charts(id);
+}
+
 function demographicPanel(id) {
     
     var result = metadata.filter(meta => meta.id.toString() === id)[0];
@@ -24,27 +29,68 @@ function demographicPanel(id) {
 
 
 function Charts(id){
-    // Build a Bubble Chart
+    
+    //Bar Chart with top 10 OTU of samples
+
+    ///Filitering throught the sample data to match the id
+    var chartSamples = samples.filter(s => s.id.toString() === id)[0];
+    //Top ten samples ranging from highest to least
+    var chartTopOTU = (samples.otu_ids.slice(0,10)).reverse();
+    //X-axis label
+    var OTUid = chartTopOTU.map(d => "OTU"+ d)
+    //Y-axis lables for top ten OTUs
+    var labels = 
+    var trace = {
+        x: sample[id].sample_values,
+        y: samples[id].otu_ids,
+        text: samples[id].otu_labels,
+        marker: {
+          color: 'rgb(142,124,195)'},
+        type:"bar",
+        orientation: "h",
+    };
+
+    // create data variable
+    var data = [trace];
+
+    // create layout variable to set plots layout
+    var layout = {
+        title: "Top 10 OTU",
+        yaxis:{
+            tickmode:"linear",
+        },
+        margin: {
+            l: 100,
+            r: 100,
+            t: 100,
+            b: 30
+        }
+    };
+
+    // create the bar plot
+    Plotly.newPlot("bar", data, layout);
+
+    // Bubble Chart
     var bubbleLayout = {
         margin: {t: 0},
         hovermode: "closest",
         xaxis: {title: "OTU ID"}
       };
-      var bubbleData = [
+    var bubbleData = [
         {
-          x: samples[0].otu_ids,
-          y: samples[0].sample_values,
-          text: samples[0].otu_labels,
+          x: samples[id].otu_ids,
+          y: samples[id].sample_values,
+          text: samples[id].otu_labels,
           mode: "markers",
           marker: {
-            size: samples[0].sample_values,
-            color: samples[0].otu_ids,
+            size: samples[id].sample_values,
+            color: samples[id].otu_ids,
             colorscale: "Earth"
             //Look up different color scales 
           }
         }
       ];
-      Plotly.plot("bubble", bubbleData, bubbleLayout);
+    Plotly.plot("bubble", bubbleData, bubbleLayout);
 };
 
 
@@ -66,7 +112,7 @@ function initialization() {
         // console.log(samples[0].otu_ids);
         // console.log(samples[0].otu_labels);
         // console.log(samples[0].sample_values);
-        Charts()
+        Charts(nameID[0])
     });
     
 };
